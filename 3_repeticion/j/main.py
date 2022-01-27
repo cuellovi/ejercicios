c59efe60b33c06d0983e9136e5515ada91b1fdad
leocuello@gmail.com <leocuello@gmail.com>
2022-01-27 12:14:37 -0300

    Fix J

--- 3_repeticion/j/main.py
+++ 3_repeticion/j/main.py
@@ -1,8 +1,8 @@
 frase = input("Ingrese una frase: ")
 letra = input("Ingrese una letra: ")
-
+total = 0
 for i in frase:
-    contador_letras = frase.count(letra)
-    print(contador_letras)
-
+    if i == letra:
+       total += 1
+print("La letra '%s' aparece %2i veces en la frase '%s'" % (letra, total, frase))
 
